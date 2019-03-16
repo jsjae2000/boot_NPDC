3e0a372fed970c37a288e4bc8ea8407d4d52c42b
jsjae2000 <jsjae2000@gmail.com>
2019-03-16 17:55:01 +0900

    simulation start

--- thesis_simul.R
+++ thesis_simul.R
@@ -282,7 +282,7 @@ MNQLE_ABoot <- function(n_boot = 500, train_model, point, bw, link_ftn = "probit
     
     # calculate bootstrap estimates
     df_lag_boot <- cmn_preproc(slice(X, 1:(d_lag + i_calboot)), Y_boot, d_lag)
-    est_boot[i_boot] <- MNQLE_optimize(x_est, z_est, x_bw, z_bw, ftns, df_lag_boot, method, init, tolerance, max_iter)
+    est_boot[i_boot] <- MNQLE_optimize(x_pt, z_pt, x_bw, z_bw, ftns, df_lag_boot, method, init, tolerance, max_iter)
     
     # print if verbose == TURE
     if (verbose) setTxtProgressBar(pb, i_boot)
@@ -295,7 +295,7 @@ MNQLE_ABoot <- function(n_boot = 500, train_model, point, bw, link_ftn = "probit
 ##### function to generate Autoregression Bootstrap samples and estimators using one-step approximation
 MNQLE_ABoot_step <- function(n_boot = 500, train_model, point, save_sample = TRUE, verbose = TRUE) {
   if (FALSE) {
-    point <- c(1, 1)
+    point <- c(-1.5, 0)
     n_boot <- 500
     train_model <- MNQLE_tr
     save_sample = TRUE
@@ -325,41 +325,59 @@ MNQLE_ABoot_step <- function(n_boot = 500, train_model, point, save_sample = TRU
   model_$point <- point
   f_hat <- do.call(MNQLE_est, model_)$reg
   
+  ### estimateion of D ###
+  list_Dhat_cal <- MNQLE_onestep_cal1(f_hat, x_pt, z_pt, x_bw, z_bw, ftns, df_lag)
+  
   ### bootstrap ###
   # calculate necessary index to calculate bootstrap samples to estimate at point
   df_conti <- df_lag[attr(df_lag, "conti_str")]
   i_calboot <- 1
   for (i in 1:ncol(df_conti)) {
-    i_calboot <- max(i_calboot, max(which((df_conti[, i] <= x_est[i] + x_bw[i]) & (df_conti[, i] >= x_est[i] - x_bw[i]))))
+    i_calboot <- max(i_calboot, max(which((df_conti[, i] <= x_pt[i] + x_bw[i]) & (df_conti[, i] >= x_pt[i] - x_bw[i]))))
   }
   
   # prepare to save bootstrap samples
-  if (save_sample) boot_sample <- matrix(NA_integer_, nrow = i_calboot + d_lag, ncol = n_boot)
+  if (save_sample) {
+    boot_sample <- matrix(NA_integer_, nrow = i_calboot + d_lag, ncol = n_boot)
+    mat_boot_mean <- matrix(NA_integer_, nrow = i_calboot, ncol = n_boot) # mean used to generate boot_sample
+  }
   if (verbose) pb <- txtProgressBar(min = 0, max = n_boot, style = 3, width = 50)
   
-  est_boot <- c()
+  est_boot <- matrix(NA_real_, nrow = n_boot, ncol = 1 + d_lag)
   for (i_boot in 1:n_boot) {
-    Y_boot <- attr(df_lag, "head")[["resp__var__"]]
+    Y_boot <- attr(df_lag, "head")[["resp__var__"]] # length  = n + d_lag
+    Y_boot_mean <- c() # vector to save m hat (Xi, ZI*); length = n
     lagresp_value <- Y_boot
     for (i_df in 1:i_calboot) {
       # generate bootstrap sample
       #lagresp_value <- rev(tail(Y_boot, d_lag))
       i_lagresp <- which(apply(attr(train_model, "df_lagresp"), 1, function(x) x == lagresp_value))
-      Y_boot_1 <- rbinom(1, 1, train_model[[i_lagresp]][i_df, 1])
+      mhat <- train_model[[i_lagresp]][i_df, 1]
+      Y_boot_1 <- rbinom(1, 1, mhat)
       lagresp_value <- c(lagresp_value[-1], Y_boot_1)
       Y_boot <- c(Y_boot, Y_boot_1)
+      Y_boot_mean <- c(Y_boot_mean, mhat)
     }
     
-    if (save_sample) boot_sample[, i_boot] <- Y_boot
+    if (save_sample) {
+      boot_sample[, i_boot] <- Y_boot
+      mat_boot_mean[, i_boot] <- Y_boot_mean
+    }
     
     # calculate bootstrap estimates
     df_lag_boot <- cmn_preproc(X[1:(i_calboot + d_lag),,drop = FALSE], Y_boot, d_lag)
+    F_hat_boot <- MNQLE_onestep_cal2(z_pt, z_bw, ftns, list_Dhat_cal, df_lag_boot, Y_boot_mean)
+    est_boot[i_boot, ] <- solve(list_Dhat_cal$nD_hat) %*% F_hat_boot
     
     # print if verbose == TURE
     if (verbose) setTxtProgressBar(pb, i_boot)
   }
   
-  if (save_sample) attr(est_boot, "samples") <- boot_sample
+  if (save_sample) {
+    attr(est_boot, "samples") <- boot_sample
+    attr(est_boot, "mat_mean") <- mat_boot_mean
+  }
+  attr(est_boot, "f_hat") <- f_hat
   return(est_boot)
 }
 
@@ -416,7 +434,7 @@ MNQLE_RBoot <- function(n_boot = 500, X, Y, estimates, d_lag, point, bw, link_ft
     
     # calculate bootstrap estimates
     df_lag_boot <- cmn_preproc(X_boot, Y_boot, d_lag)
-    est_boot[i_boot] <- MNQLE_optimize(x_est, z_est, x_bw, z_bw, ftns, df_lag_boot, method, init, tolerance, max_iter)
+    est_boot[i_boot] <- MNQLE_optimize(x_pt, z_pt, x_bw, z_bw, ftns, df_lag_boot, method, init, tolerance, max_iter)
     
     # print if verbose == TURE
     if (verbose) setTxtProgressBar(pb, i_boot)
@@ -479,7 +497,7 @@ MNQLE_bootstrap_coverage <- function(n_boot = 500, X, Y, estimates, d_lag, point
     
     # calculate bootstrap estimates
     df_lag_boot <- cmn_preproc(X_boot, Y_boot, d_lag)
-    est_boot[i_boot] <- MNQLE_optimize(x_est, z_est, x_bw, z_bw, ftns, df_lag_boot, method, init, tolerance, max_iter)
+    est_boot[i_boot] <- MNQLE_optimize(x_pt, z_pt, x_bw, z_bw, ftns, df_lag_boot, method, init, tolerance, max_iter)
     
     # print if verbose == TURE
     if (verbose) setTxtProgressBar(pb, i_boot)
@@ -491,7 +509,7 @@ MNQLE_bootstrap_coverage <- function(n_boot = 500, X, Y, estimates, d_lag, point
 }
 
 
-MNQLE_optimize <- function(x_est, z_est, x_bw, z_bw, ftns, df_lag, method = "NR", init = NULL, tolerance = 1e-05, max_iter = 1000) {
+MNQLE_optimize <- function(x_pt, z_pt, x_bw, z_bw, ftns, df_lag, method = "NR", init = NULL, tolerance = 1e-05, max_iter = 1000) {
   conti_str <- attr(df_lag, "conti_str")
   disc_str <- attr(df_lag, "disc_str")
   if (is.null(init)) init <- rnorm(1 + length(conti_str))
@@ -506,7 +524,7 @@ MNQLE_optimize <- function(x_est, z_est, x_bw, z_bw, ftns, df_lag, method = "NR"
       i_iter <- i_iter + 1
       if (i_iter > max_iter) break
       f_old <- f_new
-      NR_cal <- MNQLE_NR_calculate(f_old, x_est, z_est, x_bw, z_bw, ftns, df_lag)
+      NR_cal <- MNQLE_NR_calculate(f_old, x_pt, z_pt, x_bw, z_bw, ftns, df_lag)
       
       if (abs(det(NR_cal$eq2)) <1e-05) {
         f_new <- f_old - solve(NR_cal$eq2 + diag(diag(NR_cal$eq2), 2) * .1) %*% NR_cal$eq1  
@@ -520,7 +538,7 @@ MNQLE_optimize <- function(x_est, z_est, x_bw, z_bw, ftns, df_lag, method = "NR"
   return(list(mean = ftns$ginv(f_new[1]), reg = f_new[, 1]))
 }
 
-MNQLE_NR_calculate <- function(f_at, x_est, z_est, x_bw, z_bw, ftns, df_lag) {
+MNQLE_NR_calculate <- function(f_at, x_pt, z_pt, x_bw, z_bw, ftns, df_lag) {
   if (FALSE) {
     f_at = f_old
     x_pt = point[1:length(conti_str)]
@@ -533,13 +551,13 @@ MNQLE_NR_calculate <- function(f_at, x_est, z_est, x_bw, z_bw, ftns, df_lag) {
   conti_str <- attr(df_lag, "conti_str")
   disc_str <- attr(df_lag, "disc_str")
   
-  wci_all <- x_kern(x_bw, x_est, df_lag[conti_str])
+  wci_all <- x_kern(x_bw, x_pt, df_lag[conti_str])
   cal_ind <- wci_all != 0
   wci <- wci_all[cal_ind]
   df_cal <- df_lag[cal_ind, ]
   
-  wdi <- z_kern(z_bw, z_est, df_cal[disc_str])
-  mi <- ftns$ginv(data.matrix(sweep(df_cal[conti_str], 2, x_est)) %*%
+  wdi <- z_kern(z_bw, z_pt, df_cal[disc_str])
+  mi <- ftns$ginv(data.matrix(sweep(df_cal[conti_str], 2, x_pt)) %*%
                     matrix(f_at[-1], ncol = 1) +
                     f_at[1]
   )[, 1]
@@ -549,7 +567,7 @@ MNQLE_NR_calculate <- function(f_at, x_est, z_est, x_bw, z_bw, ftns, df_lag) {
   q1i <- (df_cal[, 1] - mi) / (ftns$Veval(mi) * ftns$gprime(mi))
   product1_i <- matrix(wci * wdi * q1i, nrow = 1)
   # dim(Wi) = n X d
-  Wi <- cbind(intercept = 1, sweep(df_cal[conti_str], 2, x_est) %>% sweep(2, FUN = "/", x_bw)) %>%
+  Wi <- cbind(intercept = 1, sweep(df_cal[conti_str], 2, x_pt) %>% sweep(2, FUN = "/", x_bw)) %>%
     as.matrix()
   # eq1_val
   eq1_val <- (product1_i %*% Wi) / nrow(df_lag)
@@ -568,18 +586,19 @@ MNQLE_NR_calculate <- function(f_at, x_est, z_est, x_bw, z_bw, ftns, df_lag) {
   return(list(eq1 = matrix(eq1_val, ncol = 1), eq2 = eq2_val))
 }
 
-MNQLE_onestep_cal1 <- function(f_hat, x_est, z_est, x_bw, z_bw, ftns, df_lag) {
+MNQLE_onestep_cal1 <- function(f_hat, x_pt, z_pt, x_bw, z_bw, ftns, df_lag) {
+  ### functions to calculate valeus used in MNQLE_ABoot_step
   conti_str <- attr(df_lag, "conti_str")
   disc_str <- attr(df_lag, "disc_str")
   
-  wci_all <- x_kern(x_bw, x_est, df_lag[conti_str])
+  wci_all <- x_kern(x_bw, x_pt, df_lag[conti_str])
   cal_ind <- wci_all != 0
   
   # From here, all objects are reduced using cal_ind
-  df_cal <- df_lag_boot[cal_ind, ]
+  df_cal <- df_lag[cal_ind, ]
   wci <- wci_all[cal_ind]
-  wdi <- z_kern(z_bw, z_est, df_cal[disc_str])
-  mi_hat <- ftns$ginv(data.matrix(sweep(df_cal[conti_str], 2, x_est)) %*%
+  wdi <- z_kern(z_bw, z_pt, df_cal[disc_str])
+  mi_hat <- ftns$ginv(data.matrix(sweep(df_cal[conti_str], 2, x_pt)) %*%
                         matrix(f_hat[-1], ncol = 1) +
                         f_hat[1]
   )[, 1]
@@ -587,55 +606,42 @@ MNQLE_onestep_cal1 <- function(f_hat, x_est, z_est, x_bw, z_bw, ftns, df_lag) {
   mi_hat[mi_hat <= 1e-05] <- 1e-05
   
   ### calculate D_hat ###
-  Wi <- cbind(intercept = 1, sweep(df_cal[conti_str], 2, x_est) %>% sweep(2, FUN = "/", x_bw)) %>%
+  Wi <- cbind(intercept = 1, sweep(df_cal[conti_str], 2, x_pt) %>% sweep(2, FUN = "/", x_bw)) %>%
     as.matrix()
-  pre_Wi <- wci * wdi / (ftns$Veval(mi_hat) * ftns$gprime(mi_hat)^2) # constants for calculating hat_D
-  nD_hat <- (sweep(t(Wi), 2, pre_Wi, FUN = "*") %*% Wi)  # n * hat_D
+  pre_Wi <- wci * wdi / (ftns$Veval(mi_hat) * ftns$gprime(mi_hat)^2) # values to calculate D_hat
+  nD_hat <- (sweep(t(Wi), 2, pre_Wi, FUN = "*") %*% Wi)  # n * D_hat
   
-  return(nD_hat)
+  return(list(nD_hat = nD_hat, wci = wci, mi_hat = mi_hat, Wi = Wi, cal_ind = cal_ind))
 }
 
-MNQLE_onestep_cal1 <- function(f_hat, x_est, z_est, x_bw, z_bw, ftns, df_lag_boot) {
+MNQLE_onestep_cal2 <- function(z_pt, z_bw, ftns, out_cal1, df_lag_boot, boot_mean) {
+  if (FALSE) {
+    tmp <- list_Dhat_cal
+    nD_hat <- tmp$nD_hat
+    wci <- tmp$wci
+    mi_hat <- tmp$mi_hat
+    Wi <- tmp$Wi
+    cal_ind <- tmp$cal_ind
+    boot_mean <- Y_boot_mean
+  }
   conti_str <- attr(df_lag_boot, "conti_str")
   disc_str <- attr(df_lag_boot, "disc_str")
   
-  wci_all <- x_kern(x_bw, x_est, df_lag_boot[conti_str])
-  cal_ind <- wci_all != 0
-  
+  wci <- out_cal1$wci
+  mi_hat <- out_cal1$mi_hat
+  Wi <- out_cal1$Wi
+  cal_ind <- out_cal1$cal_ind
+
   # From here, all objects are reduced using cal_ind
   df_cal <- df_lag_boot[cal_ind, ]
-  wci <- wci_all[cal_ind]
-  wdi <- z_kern(z_bw, z_est, df_cal[disc_str])
-  mi_hat <- ftns$ginv(data.matrix(sweep(df_cal[conti_str], 2, x_est)) %*%
-                        matrix(f_hat[-1], ncol = 1) +
-                        f_hat[1]
-  )[, 1]
-  mi_hat[mi_hat >= 1 - 1e-05] <- 1 - 1e-05
-  mi_hat[mi_hat <= 1e-05] <- 1e-05
+  wdi <- z_kern(z_bw, z_pt, df_cal[disc_str])
   
-  ### calculate D_hat ###
-  Wi <- cbind(intercept = 1, sweep(df_cal[conti_str], 2, x_est) %>% sweep(2, FUN = "/", x_bw)) %>%
-    as.matrix()
+  ### calculate F_hat_boot ###
+  preWi <- wci * wdi * (df_lag_boot$resp__var__ - boot_mean)[cal_ind] / 
+    (ftns$Veval(mi_hat) * ftns$gprime(mi_hat)) # values to calculate F_hat_boot
+  nF_hat_boot <- t(Wi) %*% matrix(preWi, ncol = 1)
   
-  q1i <- (df_cal[, 1] - mi_hat) / (ftns$Veval(mi) * ftns$gprime(mi))
-  product1_i <- matrix(wci * wdi * q1i, nrow = 1)
-  # dim(Wi) = n X d
-  
-  # eq1_val
-  eq1_val <- (product1_i %*% Wi) / nrow(df_lag)
-  
-  ### calculate eq2 val ###
-  q2i_tmp11 <- ftns$Vprime(mi) / (ftns$Veval(mi) * (ftns$gprime(mi)^2))
-  q2i_tmp12 <- ftns$gprime2(mi) / (ftns$gprime(mi))^3
-  q2i_tmp1 <- ftns$Veval(mi) / (q2i_tmp11 + q2i_tmp12)
-  q2i_tmp2 <- ftns$Veval(mi) * ftns$gprime(mi)^2; q2i_tmp2 <- 1 / q2i_tmp2
-  q2i <- -(df_cal[, 1] - mi) / q2i_tmp1 - q2i_tmp2
-  
-  product2_i <- matrix(wci * wdi * q2i, nrow = 1)
-  
-  eq2_val <- (sweep(t(Wi), 2, product2_i, FUN = "*") %*% Wi) / nrow(df_lag)
-  
-  return(list(eq1 = matrix(eq1_val, ncol = 1), eq2 = eq2_val))
+  return(nF_hat_boot)
 }
 
 plot.progress <- function(...)	{
@@ -648,6 +654,32 @@ plot.progress <- function(...)	{
   }
   title('Progress...')
 }
+
+MC_gen <- function(reg_ftn, n, y_init, x_bd, list_ftns) {
+  # start generating
+  y_vec <- c("0" = y_init)
+  mean_vec <- double()
+  x_vec = runif(n, min = x_bd[1], max = x_bd[2])
+  
+  for (i in 1:n) {
+    mean_val <- reg_ftn(x_vec[i], 
+                        y_vec[as.character(i-1)]) %>%
+      (list_ftns$ginv)
+    mean_vec <- c(mean_vec, mean_val)
+    y_vec <- c(y_vec, rbinom(1, 1, mean_val))
+    names(y_vec)[length(y_vec)] <- as.character(i)
+  }
+  
+  df_simul <- data.frame(x_1 = x_vec, 
+                         z_1 = y_vec[-length(y_vec)],
+                         y = y_vec[as.character(1:n)], 
+                         mean = mean_vec)
+  df_analysis <- df_simul %>%
+    select(x_1, y)
+  
+  return(df_analysis)
+}
+
 
 # 2. generate data -----------------------------------------------------------
 set.seed(111)
@@ -783,57 +815,99 @@ summary(df_simul$mean - df_simul$est)
 
 # 4. bootstrap -----------------------------------------------------------
 ### check : coverage of pointwise confidence interval 
-# calculate coverage for each points
-n_boot <- 500
-CI_alpha <- .05
-n_coverage <- 200
-df_CI <- df_simul[-1, ]
+n_sample <- 501
+reg_ftn <- f2
+point_est <- c(-1.5, 0)
 
-df_CI$count_coverage <- 0
-for (i_CI in 1:nrow(df_CI)) {
-  cat("point", i_CI, "starts at", as.character(Sys.time()))
-  for (i_coverage in 1:n_coverage) {
-    est_boot <- MNQLE_bootstrap1(n_boot = n_boot,
-                                 X = df_analysis[conti_str], Y = df_analysis$y, 
-                                 estimates = df_simul$est, d_lag = 1, 
-                                 point = c(df_simul[i_CI, "x_1"], df_simul[i_CI, "z_1"]), 
-                                 bw = c(x_bw_init, z_bw_init), save_sample = FALSE, verbose = FALSE)
-    est_boot <- qnorm(est_boot)
-    CI_boot <- quantile(est_boot, c(CI_alpha / 2, 1 - CI_alpha / 2))
-    
-    mean_true <- qnorm(df_CI[i_CI, "mean"])
-    if (mean_true >= CI_boot[1] & mean_true <= CI_boot[2]) df_CI[i_CI, "count_coverage"] <- df_CI[i_CI, "count_coverage"] + 1
-  }
-}
-cat("ends at", as.character(Sys.time()))
 
 # bootstrap
+n_coverage <- 150
+n_boot <- 200
+CI_alpha <- .05
+
 conti_str <- c("x_1")
 disc_str <- character(0)
 d_lag <- 1
-x_bw_init <- n^{-1/5} * 1.06 * apply(df_analysis[conti_str], 2, sd)
-z_bw_init <- n**-.4
 
-MNQLE_tr <- MNQLE_train(X = df_analysis[conti_str], Y = df_analysis$y, 
-                        d_lag = 1, bw = c(x_bw_init, z_bw_init))
+bw_factor <- 1
+count_coverage1 <- 0
+savetmp1 <- matrix(0, nrow = n_coverage, ncol = 3)
+for (i_coverage in 1:n_coverage) {
+  cat("bw_factor =", bw_factor, "/ i_coverage =", i_coverage, '\n')
+  plot.progress(i_coverage / (n_coverage * 3))
 
-boot_AR <- MNQLE_bootstrap_AR(n_boot = 500, train_model = MNQLE_tr, point = c(-1.5, 0), bw = c(x_bw_init, z_bw_init))
+  # MC data
+  df_analysis <- MC_gen(reg_ftn = reg_ftn, n = n_sample, y_init = 0, x_bd = c(-3, 3), list_ftns = cmn_defftns("probit", "binomial"))  
+  
+  # set bandwidths
+  x_bw_init <- n_sample^{-1/5} * 1.06 * apply(df_analysis[conti_str], 2, sd) * bw_factor
+  z_bw_init <- n_sample**-.4 * bw_factor
+  
+  # generate bootstrap estimators
+  MNQLE_tr <- MNQLE_train(X = df_analysis[conti_str], Y = df_analysis$y, 
+                          d_lag = 1, bw = c(x_bw_init, z_bw_init))
+  boot_AR <- MNQLE_ABoot_step(n_boot = n_boot, train_model = MNQLE_tr, point = point_est)
+  
+  # compare CI and calculate coverage
+  compare_val <- attr(boot_AR, "f_hat")[1] - reg_ftn(point_est[1], point_est[2])
+  CI_boot <- quantile(boot_AR[, 1], c(CI_alpha / 2, 1 - CI_alpha / 2))
+  
+  count_coverage1 <- count_coverage1 + unname((CI_boot[1] <= compare_val) & (CI_boot[2] >= compare_val))
+  
+  savetmp1[i_coverage, 1] <- compare_val
+  savetmp1[i_coverage, 2:3] <- CI_boot
+}
 
+bw_factor <- .75
+count_coverage2 <- 0
+savetmp2 <- matrix(0, nrow = n_coverage, ncol = 3)
+for (i_coverage in 1:n_coverage) {
+  plot.progress((i_coverage + n_coverage) / (n_coverage * 3))
+  # MC data
+  df_analysis <- MC_gen(reg_ftn = reg_ftn, n = n_sample, y_init = 0, x_bd = c(-3, 3), list_ftns = cmn_defftns("probit", "binomial"))  
+  
+  # set bandwidths
+  x_bw_init <- n_sample^{-1/5} * 1.06 * apply(df_analysis[conti_str], 2, sd) * bw_factor
+  z_bw_init <- n_sample**-.4 * bw_factor
+  
+  # generate bootstrap estimators
+  MNQLE_tr <- MNQLE_train(X = df_analysis[conti_str], Y = df_analysis$y, 
+                          d_lag = 1, bw = c(x_bw_init, z_bw_init))
+  boot_AR <- MNQLE_ABoot_step(n_boot = n_boot, train_model = MNQLE_tr, point = point_est)
+  
+  # compare CI and calculate coverage
+  compare_val <- attr(boot_AR, "f_hat")[1] - reg_ftn(point_est[1], point_est[2])
+  CI_boot <- quantile(boot_AR[, 1], c(CI_alpha / 2, 1 - CI_alpha / 2))
+  
+  count_coverage2 <- count_coverage2 + unname((CI_boot[1] <= compare_val) & (CI_boot[2] >= compare_val))
+  
+  savetmp2[i_coverage, 1] <- compare_val
+  savetmp2[i_coverage, 2:3] <- CI_boot
+}
 
-par(mfrow = c(1, 1))
-plot(filter(df_simul, z_1 == 0) %>% arrange(x_1) %>% select(x_1) %>% unlist,
-     filter(df_simul, z_1 == 0) %>% arrange(x_1) %>% select(mean) %>% unlist,
-     col = 1 + (filter(df_simul, z_1 == 0) %>% arrange(x_1) %>% select(z_1) %>% unlist()),
-     ylim = 0:1, type = "l")
-lines(filter(df_simul, z_1 == 1) %>% arrange(x_1) %>% select(x_1) %>% unlist,
-      filter(df_simul, z_1 == 1) %>% arrange(x_1) %>% select(mean) %>% unlist,
-      col = 1 + (filter(df_simul, z_1 == 1) %>% arrange(x_1) %>% select(z_1) %>% unlist()),
-      ylim = 0:1, type = "l")
-points(df_simul$x_1[-1],
-       MNQLE_tr[, 1], 
-       col = 1,
-       ylim = 0:1)
-points(df_simul$x_1[-1],
-       MNQLE_tr[, 2], 
-       col = 2,
-       ylim = 0:1)
+bw_factor <- .5
+count_coverage3 <- 0
+savetmp3 <- matrix(0, nrow = n_coverage, ncol = 3)
+for (i_coverage in 1:n_coverage) {
+  plot.progress((i_coverage + n_coverage * 2) / (n_coverage * 3))
+  # MC data
+  df_analysis <- MC_gen(reg_ftn = reg_ftn, n = n_sample, y_init = 0, x_bd = c(-3, 3), list_ftns = cmn_defftns("probit", "binomial"))  
+  
+  # set bandwidths
+  x_bw_init <- n_sample^{-1/5} * 1.06 * apply(df_analysis[conti_str], 2, sd) * bw_factor
+  z_bw_init <- n_sample**-.4 * bw_factor
+  
+  # generate bootstrap estimators
+  MNQLE_tr <- MNQLE_train(X = df_analysis[conti_str], Y = df_analysis$y, 
+                          d_lag = 1, bw = c(x_bw_init, z_bw_init))
+  boot_AR <- MNQLE_ABoot_step(n_boot = n_boot, train_model = MNQLE_tr, point = point_est)
+  
+  # compare CI and calculate coverage
+  compare_val <- attr(boot_AR, "f_hat")[1] - reg_ftn(point_est[1], point_est[2])
+  CI_boot <- quantile(boot_AR[, 1], c(CI_alpha / 2, 1 - CI_alpha / 2))
+  
+  count_coverage3 <- count_coverage3 + unname((CI_boot[1] <= compare_val) & (CI_boot[2] >= compare_val))
+  
+  savetmp3[i_coverage, 1] <- compare_val
+  savetmp3[i_coverage, 2:3] <- CI_boot
+}
